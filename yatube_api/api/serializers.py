3497bbcb60488eb7205608fc55883edc69e2d1d5
Viacheslav Borisov <borisov.slava00@yandex.ru>
2024-06-19 22:41:29 +0400

    First

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
 
-from posts.models import Comment, Post
+from posts.models import Comment, Post, Group, Follow, User
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -21,3 +21,41 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+        read_only_fields = ('post',)
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description')
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+    )
+    following = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='username',
+    )
+
+    class Meta:
+        model = Follow
+        fields = ('user', 'following')
+
+    def validate(self, data):
+        request = self.context.get('request')
+        if data['following'] == request.user:
+            raise serializers.ValidationError(
+                "Вы не можете подписаться сами на себя"
+            )
+        if Follow.objects.filter(
+            user=request.user,
+            following=data['following']
+        ):
+            raise serializers.ValidationError(
+                'Вы уже подписаны на этого юзера'
+            )
+        return data
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,58 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, filters
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import IsAuthenticated
+
+from api.serializers import (PostSerializer, GroupSerializer,
+                             CommentSerializer, FollowSerializer)
+from api.permissions import IsOwnerOrReadOnly
+from posts.models import Follow, Group, Post
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentSerializer
+
+    def get_post(self):
+        return get_object_or_404(Post, id=self.kwargs['post_id'])
+
+    def get_queryset(self):
+        post = self.get_post()
+        return post.comments.all()
+
+    def perform_create(self, serializer):
+        post = self.get_post()
+
+        serializer.save(author=self.request.user, post=post)
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('following__username', 'user__username')
+
+    def get_queryset(self):
+        new_queryset = Follow.objects.filter(user=self.request.user)
+        return new_queryset
+
+    def perform_create(self, serializer):
+        serializer.save(
+            user=self.request.user,
+        )
